d774f55d9acb556de96a3f7dcb24cfd10aa16cdc
sys_gfxdevsh <x.wang@intel.com>
2023-11-23 03:31:15 +0000

    draft design for the whole structure of kernel

---
+++ kernels/include/primitive.hpp
@@ -0,0 +1,13 @@
+#include "primitive_desc.hpp"
+#include "primitive_config.hpp"
+
+namespace prim {
+/* Primitive Class (TODO)
+ * 1) base class, integrate all classes as member(init/set/get):
+ * primitive desc, param config, primitive config, stream, memory, primitive factory
+ * 2) select best fit impl of operator
+ * 3) execute primitive
+ * */
+class primitive {
+};
+}
---
+++ kernels/include/queue.hpp
@@ -0,0 +1,16 @@
+#include <queue>
+
+/* Stream (TODO)
+ * Work queue for GPU and CPU
+ * Provide base API:
+ * 1) Submit func
+ * 2) Wait func
+ * 3) Sync func
+ * 4) Event watcher
+ */
+namespace stream {
+class stream {
+
+};
+
+}
