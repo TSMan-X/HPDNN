154a9fa5b36608465ca749868a19cb2d1b82a340
jonahmoon <jonahmoon@github.com>
2023-12-06 07:02:45 +0800

    update

--- kernels/src/softmax_base.cu
+++ kernels/src/softmax_base.cu
@@ -1,44 +1,95 @@
-// Only support softmax operation at dim 1.
 template<class T>
-__global__ void softmax_base_2D(T * input, T * output, T * tmp, int M, int N, int size) {
+__global__ void reduce_max_2D(T * input, T * tmp, int M, int N) {
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
   int gx = threadIdx.x + blockIdx.x * blockDim.x;
   int gy = threadIdx.y + blockIdx.y * blockDim.y;
 
-  // 1. reduceMax
-  if (gx < N && gx < M) {
-    tmp[gy * N + gx] = input[gy * N + gx];
+  extern __shared__ T shared[];
+  if (gx < N && gy < M) {
+    shared[ty * blockDim.x + tx] = input[gy * N + gx];
   }
+  __syncthreads();
+
+  for (int s = blockDim.x / 2; s > 0; s >>= 1) { // calculate the max value of a row, and will be stored at shared[y][0]
+    if ( tx < s && gy < M ) {
+      shared[ty * blockDim.x + tx] = max(shared[ty * blockDim.x + tx + s], shared[ty * blockDim.x + tx]); 
+    }
+    __syncthreads();
+  }
+
+  if (tx == 0) {
+    atomicMax(tmp + gy, shared[0]);
+  }
+}
+
+template<class T>
+__global__ void broadcast_sub_elementwise_exp(T * input, T * output, T * tmp, int M, int N){
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if ( gx < N && gy < M ) {
+    output[gy * N + gx] = __expf(input[gy * N + gx] - tmp[gy]);
+  }
+}
+
+template<class T>
+__global__ void reduce_sum_2D(T * output, T * tmp, int M, int N){
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  extern __shared__ T shared[];
+  if (gx < N && gy < M) {
+    shared[ty * blockDim.x + tx] = output[gy * N + gx];
+  }
+  __syncthreads();
+
+  for (int s = blockDim.x / 2; s > 0; s >>= 1) { // calculate the max value of a row, and will be stored at shared[y][0]
+    if ( tx < s && gy < M ) {
+      shared[ty * blockDim.x + tx] = shared[ty * blockDim.x + tx + s] + shared[ty * blockDim.x + tx]; 
+    }
+    __syncthreads();
+  }
+
+  if (tx == 0) {
+    atomicAdd(tmp + gy, shared[0]);
+  }
+}
+
+template<class T>
+__global__ void broadcast_div(T * output, T * tmp, int M, int N){
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if ( gx < N && gy < M ) {
+    output[gy * N + gx] = output[gy * N + gx] / tmp[gy];
+  }
+}
+
+// Only support softmax operation at dim 1.
+template<class T>
+void softmax_base_2D(T * input, T * output, T * tmp, int M, int N, const dim3 & grid, const dim3 & block) {
+  // input: M * N
+  // output: M * N
+  // tmp : M
+
+  // 1. reduceMax
+  reduce_max_2D<DT><<<grid, block>>>(input, tmp, M, N);
   cudaDeviceSynchronize();
 
-  for (int s = N / 2; s > 0; s >>= 1) { // calculate the max value of a row, and will be stored at tmp[y][0]
-    if ( gx < s && gy < M ) {
-      tmp[gy * N + gx] = max(tmp[gy * N + gx + s], tmp[gy * N + gx]); 
-    }
-    cudaDeviceSynchronize();
-  }
-
   // 2. broadcastSub and elementwiseExp
-  if ( gx < N && gy < M ) {
-    output[gy * N + gx] = __expf(input[gy * N + gx] - tmp[gy * N]);
-  }
+  broadcast_sub_elementwise_exp<DT><<<grid, block>>>(input, output, tmp, M, N);
   cudaDeviceSynchronize();
 
   // 3. reduceSum
-  if (gx < N && gx < M) {
-    tmp[gy * N + gx] = output[gy * N + gx];
-  }
+  reduce_sum_2D<DT><<<grid, block>>>(input, tmp, M, N);
   cudaDeviceSynchronize();
 
-  for (int s = N / 2; s > 0; s >>= 1) { // calculate the sum value of a row, and will be stored at tmp[y][0]
-    if ( gx < s && gy < M ) {
-      tmp[gy * N + gx] = tmp[gy * N + gx + s] + tmp[gy * N + gx]; 
-    }
-    cudaDeviceSynchronize();
-  }
-
   // 4. broadcastDiv
-  if ( gx < N && gy < M ) {
-    output[gy * N + gx] = output[gy * N + gx] / tmp[gy * N];
-  }
+  broadcast_div<DT><<<grid, block>>>(output, tmp, M, N);
+  cudaDeviceSynchronize();
 
+  return;
 }
