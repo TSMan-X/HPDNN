0433ae73321416c4e576e2209c21d5e5564c0c34
majiangn <jonahmoon@hotmail.com>
2023-12-06 13:09:18 +0800

    softmax base (#9)
    softmax base version
    
    Co-authored-by: jonahmoon <jonahmoon@github.com>

---
+++ kernels/sample/softmax_2D.cu
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cuda_runtime.h>
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include "../src/softmax_base.cu"
+
+
+#define M 1024
+#define N 1024
+
+#ifdef INT
+  #define DT int
+#elif FLOAT
+  #define DT float
+#else
+  #define DT int
+#endif
+
+
+// CPU softmax
+void cpu_softmax(DT* input, DT* output, int m, int n) {
+  int * tmp = new DT[m];
+
+  // max
+  std::fill(tmp, tmp + m, std::numeric_limits<DT>::min());
+  for (int idx_m = 0; idx_m < m; ++idx_m) {
+    for (int idx_n = 0; idx_n < n; ++idx_n) {
+       if (input[idx_m * n + idx_n] > tmp[idx_m]) tmp[idx_m] = input[idx_m * n + idx_n];
+    }
+  }
+
+  // sub and exp
+  for (int idx_m = 0; idx_m < m; ++idx_m) {
+    for (int idx_n = 0; idx_n < n; ++idx_n) {
+       output[idx_m * n + idx_n] = std::exp(input[idx_m * n + idx_n] - tmp[idx_m]);
+    }
+  }
+
+  // sum
+  std::fill(tmp, tmp + m, 0);
+  for (int idx_m = 0; idx_m < m; ++idx_m) {
+    for (int idx_n = 0; idx_n < n; ++idx_n) {
+       tmp[idx_m] += output[idx_m * n + idx_n];
+    }
+  }
+
+  // div
+  for (int idx_m = 0; idx_m < m; ++idx_m) {
+    for (int idx_n = 0; idx_n < n; ++idx_n) {
+       output[idx_m * n + idx_n] = output[idx_m * n + idx_n] / tmp[idx_m];
+    }
+  }
+
+}
+
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    
+  }
+  int blockSizeM = 32;
+  int blockSizeN = 32;
+  int threadSizeM = 1;
+  int threadSizeN = 1;
+
+  DT *A = new DT[M * N];
+  DT *B = new DT[M * N];
+  DT *T = new DT[M];
+  std::fill_n(A, M * N, 1);
+  std::fill_n(B, M * N, 1);
+  std::fill_n(T, M, 0);
+  
+  //  CPU softmax
+  cpu_softmax(A, B, M, N);
+  
+  //  GPU softmax
+  //  init device
+  int dev = 0;
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, dev);
+  cudaSetDevice(dev);
+  
+  //  set block and thread size
+  dim3 grid(M/blockSizeM, N/blockSizeN);
+  dim3 block(blockSizeM / threadSizeM, blockSizeN / threadSizeN);
+  
+  //  alloc device memory
+  DT *D_A, *D_B, *D_T;
+  cudaMalloc((DT**)(&D_A), M * N * sizeof(DT));
+  cudaMalloc((DT**)(&D_B), M * N * sizeof(DT));
+  cudaMalloc((DT**)(&D_T), M * sizeof(DT));
+
+  cudaMemcpy(D_A, A, M * N * sizeof(DT), cudaMemcpyHostToDevice);
+
+  softmax_base_2D<DT>(D_A, D_B, D_T, M, N, grid, block);
+  cudaDeviceSynchronize();
+  cudaError_t cudaError = cudaGetLastError();
+  if (cudaError != cudaSuccess) {
+    //  Error check
+    std::cerr << "CUDA kernel error: " << cudaGetErrorString(cudaError) << std::endl;
+  }
+
+  //  Correctness check
+  DT *H_B = new DT[M * N];
+  cudaMemcpy(H_B, D_B, N * M * sizeof(DT), cudaMemcpyDeviceToHost);
+
+  bool flag = true;
+  for (int i = 0; i < M; ++i) {
+    if (flag)
+      for (int j = 0; j < N; ++j) {
+        if (H_B[i * N + j] != B[i * N + j]) {
+          std::cout << "wrong" << std::endl;
+          std::cout << H_B[i * N + j]  << " " << i << " " << j << std::endl;
+          flag = false;
+          break;
+        }
+      }
+  }
+  
+  delete A;
+  delete B;
+  delete T;
+  delete H_B;
+
+}
---
+++ kernels/src/softmax_base.cu
@@ -0,0 +1,91 @@
+template<class T>
+__global__ void reduce_max_2D(T * input, T * tmp, int M, int N) {
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  extern __shared__ T shared[];
+  if (gx < N && gy < M) {
+    shared[ty * blockDim.x + tx] = input[gy * N + gx];
+  }
+  __syncthreads();
+
+  for (int s = blockDim.x / 2; s > 0; s >>= 1) { // calculate the max value of a row, and will be stored at shared[y][0]
+    if ( tx < s && gy < M ) {
+      shared[ty * blockDim.x + tx] = max(shared[ty * blockDim.x + tx + s], shared[ty * blockDim.x + tx]); 
+    }
+    __syncthreads();
+  }
+
+  if (tx == 0) {
+    atomicMax(tmp + gy, shared[0]);
+  }
+}
+
+template<class T>
+__global__ void broadcast_sub_elementwise_exp(T * input, T * output, T * tmp, int M, int N){
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if ( gx < N && gy < M ) {
+    output[gy * N + gx] = __expf(input[gy * N + gx] - tmp[gy]);
+  }
+}
+
+template<class T>
+__global__ void reduce_sum_2D(T * output, T * tmp, int M, int N){
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  extern __shared__ T shared[];
+  if (gx < N && gy < M) {
+    shared[ty * blockDim.x + tx] = output[gy * N + gx];
+  }
+  __syncthreads();
+
+  for (int s = blockDim.x / 2; s > 0; s >>= 1) { // calculate the max value of a row, and will be stored at shared[y][0]
+    if ( tx < s && gy < M ) {
+      shared[ty * blockDim.x + tx] = shared[ty * blockDim.x + tx + s] + shared[ty * blockDim.x + tx]; 
+    }
+    __syncthreads();
+  }
+
+  if (tx == 0) {
+    atomicAdd(tmp + gy, shared[0]);
+  }
+}
+
+template<class T>
+__global__ void broadcast_div(T * output, T * tmp, int M, int N){
+  int gx = threadIdx.x + blockIdx.x * blockDim.x;
+  int gy = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if ( gx < N && gy < M ) {
+    output[gy * N + gx] = output[gy * N + gx] / tmp[gy];
+  }
+}
+
+// Only support softmax operation at dim 1.
+template<class T>
+void softmax_base_2D(T * input, T * output, T * tmp, int M, int N, const dim3 & grid, const dim3 & block) {
+  // input: M * N
+  // output: M * N
+  // tmp : M
+
+  // 1. reduceMax
+  reduce_max_2D<T><<<grid, block, block.x * block.y * sizeof(T)>>>(input, tmp, M, N);
+
+  // 2. broadcastSub and elementwiseExp
+  broadcast_sub_elementwise_exp<T><<<grid, block>>>(input, output, tmp, M, N);
+
+  // 3. reduceSum
+  reduce_sum_2D<T><<<grid, block, block.x * block.y * sizeof(T)>>>(input, tmp, M, N);
+
+  // 4. broadcastDiv
+  broadcast_div<T><<<grid, block>>>(output, tmp, M, N);
+  
+  return;
+}
